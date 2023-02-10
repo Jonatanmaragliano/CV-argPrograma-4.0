9aafc562643f4bf13fb82461d4d801dca7f05a42
Jonatan Maragliano <jonatanmaragliano@gmail.com>
2023-02-10 17:41:37 -0300

    Mi primer commit en la pagina, TP 1 terminado

---
+++ root/cv.js
@@ -0,0 +1,4 @@
+document.getElementById("button").addEventListener('click', function(){
+    document.getElementById("demo").className="quien-soy"
+    document.getElementById("demo").innerHTML="<p></p>Si estas leyendo esta parte, es porque te interesa saber queine soy. La verdad es que soy una persona comun y corriente, inquieto, buscando siempre algo par aprender y crecer. Me gusta interactuar mucho con personas y construien en equipo. Futuro padre de GIO y en pareja con Ornela, buscando momentos para disfrutr juntos, que eso para mi es lo más importante en la vida!!!</p>"   
+});
